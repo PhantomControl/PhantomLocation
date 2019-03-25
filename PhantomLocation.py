e42cf7a6c6dcc48c84157e4260cebd41e588d507
PhantomControl <48925321+PhantomControl@users.noreply.github.com>
2019-03-25 16:59:24 -0500

    Update and rename PhantomControl.py to PhantomLocation.py

--- PhantomLocation.py
+++ PhantomLocation.py
@@ -11,52 +11,89 @@ colorama.init(autoreset=True)
 print(Fore.MAGENTA + "Please enter image name to begin.")
 simage = input('')
 
+
 def mainMenu():
     banner = pyfiglet.figlet_format("PhantomLocation", font="rectangles")
+
     print(Fore.MAGENTA + banner)
+
     print("+-+" * 22)
+
     print(Fore.MAGENTA + "[1] Extract Exif data")
+
     print(Fore.MAGENTA + "[2] Obtain location data")
+
     print(Fore.MAGENTA + "[3] Exit")
+
     while True:
         try:
+
             selection = int(input(Fore.MAGENTA + "Enter choice:>> "))
+
             if selection == 99:
+
                 crt()
+
                 break
-                #Debug command for checking selected image
+
+                # Debug command for checking selected image
+
             elif selection == 1:
+
                 edata()
+
                 break
+
             elif selection == 2:
+
                 ldata()
+
                 break
+
             elif selection == 3:
+
                 break
+
             else:
+
                 print("Invalid choice. Enter 1-3")
+
                 mainMenu()
+
         except ValueError:
+
             print(Fore.RED + "Invalid choice. Enter 1-3")
-    exit()
+
+        exit()
+
 
 def crt():
     print(simage)
+
     anykey = input(Fore.MAGENTA + "Enter anything to return to the main menu")
+
     mainMenu()
+
 
 def sphoto():
     print(Fore.MAGENTA + "Select Photo")
+
     simage = input()
+
     mainMenu()
+
 
 def edata():
     print(Fore.RED + "Extracting Exif Data")
+
     print(exif)
+
     print(labeled)
+
     print(get_coordinates(geotags))
 
     anykey = input(Fore.MAGENTA + "Enter anything to return to the main menu")
+
     mainMenu()
 
 
@@ -64,6 +101,7 @@ def ldata():
     print(Fore.RED + "Obtaining Location Data")
 
     key = 'fe27ab7fb62a4cffaeaee666a642d749'
+
     geocoder = OpenCageGeocode(key)
 
     print(Fore.MAGENTA + "Input latitude")
@@ -77,27 +115,31 @@ def ldata():
     print(Fore.RED + "*Obtaining Location Data*")
 
     results = geocoder.reverse_geocode(lat, lon)
+
     pprint(results)
 
     anykey = input(Fore.MAGENTA + "Enter anything to return to the main menu")
+
     mainMenu()
-
-
-
 
 
 def get_exif(filename):
     image = Image.open(filename)
+
     image.verify()
+
     return image._getexif()
 
 
 exif = get_exif(simage)
-#print(exif)
+
+
+# print(exif)
 
 
 def get_labeled_exif(exif):
     labeled = {}
+
     for (key, val) in exif.items():
         labeled[TAGS.get(key)] = val
 
@@ -105,8 +147,11 @@ def get_labeled_exif(exif):
 
 
 exif = get_exif(simage)
+
 labeled = get_labeled_exif(exif)
-#print(labeled)
+
+
+# print(labeled)
 
 
 def get_geotagging(exif):
@@ -114,12 +159,16 @@ def get_geotagging(exif):
         raise ValueError("No EXIF metadata found")
 
     geotagging = {}
+
     for (idx, tag) in TAGS.items():
+
         if tag == 'GPSInfo':
+
             if idx not in exif:
                 raise ValueError("No EXIF geotagging found")
 
             for (key, val) in GPSTAGS.items():
+
                 if key in exif[idx]:
                     geotagging[val] = exif[idx][key]
 
@@ -127,18 +176,25 @@ def get_geotagging(exif):
 
 
 exif = get_exif(simage)
+
 geotags = get_geotagging(exif)
-#print(geotags)
+
+
+# print(geotags)
 
 
 def get_decimal_from_dms(dms, ref):
     degrees = dms[0][0] / dms[0][1]
+
     minutes = dms[1][0] / dms[1][1] / 60.0
+
     seconds = dms[2][0] / dms[2][1] / 3600.0
 
     if ref in ['S', 'W']:
         degrees = -degrees
+
         minutes = -minutes
+
         seconds = -seconds
 
     return round(degrees + minutes + seconds, 5)
@@ -153,7 +209,10 @@ def get_coordinates(geotags):
 
 
 exif = get_exif(simage)
+
 geotags = get_geotagging(exif)
-#print(get_coordinates(geotags))
+
+# print(get_coordinates(geotags))
+
 
 mainMenu()
